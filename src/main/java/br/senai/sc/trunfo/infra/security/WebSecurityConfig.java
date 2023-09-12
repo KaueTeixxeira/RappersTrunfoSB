f2417945f7c58dc603625345f0e1b10757d6a897
KaueTeixxeira <kaueteixerak9@hotmail.com>
2023-09-12 13:40:22 -0300

    Spring security com apenas uma classe contendo UserDatails

--- src/main/java/br/senai/sc/trunfo/infra/security/WebSecurityConfig.java
+++ src/main/java/br/senai/sc/trunfo/infra/security/WebSecurityConfig.java
@@ -15,6 +15,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 import static org.springframework.security.config.Customizer.*;
 
@@ -45,5 +50,18 @@ public class WebSecurityConfig  {
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://localhost:4200")); // Trocar a porta, caso precise
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+        configuration.setAllowCredentials(true);
+        configuration.addAllowedHeader("*");
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
 
 }
--- src/main/java/br/senai/sc/trunfo/service/CartaService.java
+++ src/main/java/br/senai/sc/trunfo/service/CartaService.java
@@ -21,9 +21,7 @@ import java.util.Optional;
 @AllArgsConstructor
 public class CartaService {
 
-
     private CartaRepository cartaRepository;
-
 
     public Carta create(Carta carta) {
         return cartaRepository.save(carta);
